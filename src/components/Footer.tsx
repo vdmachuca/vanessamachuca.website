f3816326963517c2c37827b5f2622bdf868375b5
Vanessa Machuca <31165097+vdmachuca@users.noreply.github.com>
2026-03-02 13:05:33 -0800

    updated with my info

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -7,10 +7,12 @@ function Footer() {
   return (
     <footer>
       <div>
-        <a href="https://github.com/yujisatojr" target="_blank" rel="noreferrer"><GitHubIcon/></a>
-        <a href="https://www.linkedin.com/in/yujisato/" target="_blank" rel="noreferrer"><LinkedInIcon/></a>
+        <a href="https://github.com/vdmachuca" target="_blank" rel="noreferrer"><GitHubIcon/></a>
+        <a href="https://www.linkedin.com/in/vanessa-machuca/" target="_blank" rel="noreferrer"><LinkedInIcon/></a>
       </div>
-      <p>A portfolio designed & built by <a href="https://github.com/yujisatojr/react-portfolio-template" target="_blank" rel="noreferrer">Yuji Sato</a> with 💜</p>
+      <p>Created by Vanessa Machuca
+        <br/>Original website template by <a href="https://github.com/yujisatojr/react-portfolio-template" target="_blank" rel="noreferrer" style={{color: '#2E7DB8'}}>Yuji Sato</a>
+      </p>    
     </footer>
   );
 }
